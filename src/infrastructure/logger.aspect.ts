13dfe5bd6e5f5cc3a6d4200d6839b2524772f45f
Daniel Clarke <Clarkit@gmail.com>
2021-10-18 14:59:57 +0100

    Created singleton logger instance for aspect logger

--- src/infrastructure/logger.aspect.ts
+++ src/infrastructure/logger.aspect.ts
@@ -2,12 +2,21 @@ import {beforeMethod, Metadata} from 'aspect.js';
 import {Logger} from '@nestjs/common';
 
 export class AspectLogger {
+  private static instance: Logger;
+  private getInstance(): Logger {
+    if (!AspectLogger.instance) {
+      console.log('Constructing logger');
+      AspectLogger.instance = new Logger(AspectLogger.name);
+    }
+    return AspectLogger.instance;
+  }
+
   @beforeMethod({
     classNamePattern: /.*/,
     methodNamePattern: /.*/,
   })
   invokeBeforeMethod(meta: Metadata) {
-    new Logger(AspectLogger.name).log(
+    this.getInstance().log(
       `Inside of the logger. Called ${meta.className}.${
         meta.method.name
       } with args: ${JSON.stringify(meta.method.args)}.`
