46290b07bab5e2fd8d5427bf97a02c9624e14ebd
Daniel Clarke <Clarkit@gmail.com>
2021-10-18 13:10:45 +0100

    added request id header middleware

--- src/resource/main.ts
+++ src/resource/main.ts
@@ -2,9 +2,12 @@ import {NestFactory} from '@nestjs/core';
 import {Logger} from 'nestjs-pino';
 import {AppModule} from './app.module';
 
+import * as addRequestIdMiddleware from 'express-request-id';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {bufferLogs: true});
   app.useLogger(app.get(Logger));
+  app.use(addRequestIdMiddleware());
   await app.listen(3000);
 }
 bootstrap();
