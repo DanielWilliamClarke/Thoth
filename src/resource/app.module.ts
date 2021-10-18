1711eee8616ffa906c7541e5da066e8be5297ace
Daniel Clarke <Clarkit@gmail.com>
2021-10-18 15:43:41 +0100

    added pino-stackdriver to mold pino logs into stackdriver format

--- src/resource/app.module.ts
+++ src/resource/app.module.ts
@@ -1,11 +1,16 @@
 import {Logger, Module} from '@nestjs/common';
+import {createStream} from '@binxhealth/pino-stackdriver';
 import {LoggerModule} from 'nestjs-pino';
 import {Command, DataAccess, Repository} from '../domain';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 
 @Module({
-  imports: [LoggerModule.forRoot()],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: [{}, createStream()],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, Command, Repository, DataAccess, Logger],
 })
